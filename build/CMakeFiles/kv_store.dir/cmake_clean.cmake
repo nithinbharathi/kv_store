3a8a6a9b02ac038153fad04de4003c8a65b56291
nithinbharathi <nithinbharathik@outlook.com>
2025-08-04 18:20:19 -0300

    implemented put operation

---
+++ build/CMakeFiles/kv_store.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kv_store.dir/src/kv_store.o"
+  "CMakeFiles/kv_store.dir/src/kv_store.o.d"
+  "kv_store"
+  "kv_store.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/kv_store.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
