208a3f5da26091102c41d9d0081ae8b8e934ec30
nithinbharathi <nithinbharathik@outlook.com>
2025-08-05 15:07:16 -0300

    added test method for get

--- tests/test.c
+++ tests/test.c
@@ -4,6 +4,8 @@
 #include<stdio.h>
 #include<string.h>
 
+const size_t key_len = 64, val_len = 128;
+
 void get_random_str(char *s, const size_t len){
     static char *str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     for(int i = 0;i<len;i++){
@@ -12,27 +14,47 @@ void get_random_str(char *s, const size_t len){
     s[len] = '\0';
 }
 
-int main(){
-    int ops = 10000;
+void benchmark_put(char key[][key_len+1], char val[][val_len+1], int epochs){
     double time_taken = 0.0;
-    const size_t key_len = 64, val_len = 128;
-    char key[key_len+1], val[val_len+1];
     struct timespec start, end;
-    srand(time(NULL));
 
-    for(int i = 0;i<ops;i++){
-        get_random_str(key, key_len);
-        get_random_str(val, val_len);
+    for(int i = 0;i<epochs;i++){
 
         clock_gettime(CLOCK_MONOTONIC, &start);
-        put(key, val);
+        put(key[i], val[i]);
         clock_gettime(CLOCK_MONOTONIC, &end);
 
         time_taken += ((end.tv_nsec - start.tv_nsec) + (end.tv_sec - start.tv_sec))/1e9;
     }
 
-    printf("avg time for put invoked %d times %.6f", ops, time_taken/ops);
-    print();
+    printf("avg time for put invoked %d times %.6f\n", ops, time_taken/ops);
+}
 
+void benchmark_get(char key[][key_len+1], int epochs){
+    double time_taken = 0.0;
+    struct timespec start, end;
+
+    for(int i = 0;i<epochs;i++){
+        clock_gettime(CLOCK_MONOTONIC, &start);
+        get(key[i]);
+        clock_gettime(CLOCK_MONOTONIC, &end);
+
+        time_taken += ((end.tv_nsec - start.tv_nsec) + (end.tv_sec - start.tv_sec))/1e9;
+    }
+
+    printf("avg time for get invoked %d times %.6f\n", ops, time_taken/ops);
+}
+
+int main(){
+    int epochs = 100;
+    char keys[epochs][key_len+1], vals[epochs][val_len+1];
+    srand(time(NULL));
+
+    for(int i = 0;i<epochs;i++){
+        get_random_str(keys[i], key_len);
+        get_random_str(vals[i], val_len);
+    }
+
+    benchmark_put(keys, vals, epochs);
     return 0;
 }
