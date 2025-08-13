a0da00e0b88d43b05b892344ff717c3570934e9b
nithinbharathi <nithinbharathik@outlook.com>
2025-08-13 16:10:47 -0300

    implemented event loop to handle remote connections

---
+++ include/event_loop.h
@@ -0,0 +1,7 @@
+#ifndef _EVENT_LOOP_H_
+#define _EVENT_LOOP_H_
+
+void handle_sigint(int sig);
+void event_loop();
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,43 +1,12 @@
-#include<string.h>
 #include<stdio.h>
-#include<ctype.h>
-#include<sys/stat.h>
-#include "kv_store.h"
 #include "utils.h"
-#include<errno.h>
+#include "event_loop.h"
 
-#define PATH "/home/.kv_store"
 
 int main(){
-    printf("***********************************************************************************\n");
-    char line[50];
+    printf("***********************************************************************************\n"); 
     create_directory(get_dir_path());
-
-    while(fgets(line, sizeof(line), stdin)){
-        line[strcspn(line, "\n")] = '\0';
-        char *command = strtok(line," ");
-        to_lower(command);
-
-       if(strcmp(command, "put") == 0){
-            char *key = strtok(NULL, " ");
-            char *val = strtok(NULL, "");
-
-            if(key == NULL || val == NULL){
-                fprintf(stderr, "Key or value cannot NULL\n");
-                continue;
-            }
-            put(key,val);
-            printf("added\n");
-       }else if(strcmp(command, "print") == 0){
-            print();
-       }else if (strcmp(command, "get") == 0){
-            char *key = strtok(NULL, " ");
-            printf("%s\n", get(key));
-       }
-       else{
-            printf("invalid command\n");
-       }
-       printf("\n");
-    }
+    event_loop();
+    
     return 0;
 }
